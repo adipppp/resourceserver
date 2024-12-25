c2ba432dcec19e37dfe7a5cd46c15e650c99b6d0
adipppp <adhif2004@gmail.com>
2024-12-25 12:49:30 +0700

    feat: Add MessageService

---
+++ src/main/java/com/stackjs/resourceserver/service/MessageService.java
@@ -0,0 +1,28 @@
+package com.stackjs.resourceserver.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.stackjs.resourceserver.model.Message;
+import com.stackjs.resourceserver.repository.MessageRepository;
+
+@Service
+public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public List<Message> findAll() {
+        return messageRepository.findAll();
+    }
+
+    public Optional<Message> findById(long id) {
+        return messageRepository.findById(id);
+    }
+
+}
