120c8d3176d26cac8432b7da0a510f9ac10dfa19
adipppp <adhif2004@gmail.com>
2024-12-26 15:47:14 +0700

    feat: Add endpoint for posting messages in MessageController

--- src/main/java/com/stackjs/resourceserver/controller/MessageController.java
+++ src/main/java/com/stackjs/resourceserver/controller/MessageController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +36,11 @@ public class MessageController {
             throw new IllegalArgumentException("Id is not a number: " + id);
         }
         return messageService.findById(idInt64).get();
+    }
+
+    @PostMapping({"", "/"})
+    public Message save(@RequestBody Message message) {
+        return messageService.save(message);
     }
 
 }
